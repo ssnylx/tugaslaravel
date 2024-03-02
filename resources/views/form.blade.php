a01aaf162ab91ba5232bc2d8b0665f67d5ec2103
sinta <ssintanila@gmail.com>
2024-03-02 18:19:08 +0700

    project5 update

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,31 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tambah Data Baru</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <form action="/data-baru" method="POST">
         @csrf
-        <div>
+        <div class="mb-3">
             <label for="">NIM</label>
             <input type="number" name="nim" id="">
         </div>
-        <div>
+        <div class="mb-3" >
             <label for="">Nama</label>
             <input type="text" name="nama" id="">
         </div>
-        <div>
+        <div class="mb-3">
             <label for="">Prodi</label>
             <input type="text" name="prodi" id="">
         </div>
-        <div>
+        <div class="mb-3">
             <label for="">Fakultas</label>
             <input type="text" name="fakultas" id="">
         </div>
-        <div>
+        <div class="mb-3">
             <label for="">Jenis Kelamin</label>
             <input type="text" name="jenis_kelamin" id="" placeholder="P / L">
         </div>
         <button type="submit">Tambah Data</button>
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/tampil.blade.php
+++ resources/views/tampil.blade.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>View Data Mahasiswa</title>
-    <style>
-        table {
-            margin: 0px auto;
-            width: 80%;
-            text-align: center;
-        } tr, td, th {
-            border: 1px solid black;
-        }
-    </style>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
 </head>
 <body>
+
     <a href="/data-mahasiswa"> Tambah Data Baru </a>
-    <table>
+    <table class="table table-striped">
         <thead>
             <th> NIM </th>
             <th> Nama </th>
@@ -37,5 +33,8 @@
             @endforeach
         </tbody>
     </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
